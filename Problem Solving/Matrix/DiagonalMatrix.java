b92cb1424c1f2a2cc0d3304ed21052702b68f322
Aayu8982 <aayu8982@gmail.com>
2022-09-06 23:01:25 +0530

    Matrix basics cleared

--- Problem Solving/Matrix/DiagonalMatrix.java
+++ Problem Solving/Matrix/DiagonalMatrix.java
@@ -1,3 +1,16 @@
+/* Create Diagonal Matrix
+* arr= [1 2 3 4 5]
+* 
+* output
+* 1 0 0 0 0 
+* 0 2 0 0 0 
+* 0 0 3 0 0 
+* 0 0 0 4 0 
+* 0 0 0 0 5 
+*/
+
+
+
 import java.util.*;
 class DiagonalMatrix{
 
--- Problem Solving/Matrix/MatrixMultiplication.java
+++ Problem Solving/Matrix/MatrixMultiplication.java
@@ -1,3 +1,21 @@
+/* Multiplication of 2 Matrix
+* first
+*  2 3
+* 1 2 3
+* 4 5 6
+*
+* second
+* 3 4
+* 1 2 3 4
+* 5 6 7 8
+* 9 10 11 12
+*
+* output 
+* 38 44 50 56 
+* 83 98 113 128 
+*/
+
+
 import java.util.*;
 
 class MatrixMultiplication{
